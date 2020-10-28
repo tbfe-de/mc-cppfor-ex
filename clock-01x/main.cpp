a02726685b9691bdcc91172bdd2704a9773f4afa
Martin Weitzel <mw@tbfe.de>
2020-10-28 06:35:14 +0100

    Snapshot 2020-10-28 06:35 (MW)

---
+++ clock-01x/main.cpp
@@ -0,0 +1,41 @@
+#include "Clock.h"
+
+#include <iostream>
+#include <sstream>
+
+#define SHOW_(expect, ...)\
+    do {\
+        std::ostringstream result;\
+	result.copyfmt(std::cout);\
+	result << (__VA_ARGS__);\
+       	std::cout << __FILE__ <<  ':' << __LINE__ << '\t'\
+                  << #__VA_ARGS__ << " --> " << result.str();\
+	if (result.str() != expect) {\
+		std::cout << " != " << expect;\
+	}\
+	std::cout << std::endl;\
+    }\
+    while (0)
+
+int main()
+{
+    std::cout.setf(std::ios::boolalpha);
+    
+    Clock c{};              SHOW_("00:00:00", c);
+    c.TickUp();             SHOW_("00:00:01", c);
+    c.TickUp();             SHOW_("00:00:02", c);
+    c.TickUp();             SHOW_("00:00:03", c);
+
+    c.Set(0, 0, 58);	    SHOW_("00:00:58", c);
+    c.TickUp();             SHOW_("00:00:59", c);
+    c.TickUp();             SHOW_("00:01:00", c);
+    c.TickUp();             SHOW_("00:01:01", c);
+    c.TickUp(60);           SHOW_("00:02:01", c);
+    c.TickUp(100);          SHOW_("00:03:41", c);
+    c.TickUp(1000);         SHOW_("00:20:21", c);
+    
+    Clock c2{23, 59, 58};   SHOW_("23:59:58", c2);
+    c2.TickUp();            SHOW_("23:59:59", c2);
+    c2.TickUp();            SHOW_("00:00:00", c2);
+    c2.TickUp();            SHOW_("00:00:01", c2);
+ }
--- clock-07/README.txt
+++ clock-07/README.txt
@@ -2,4 +2,11 @@ A Clock That Can Tick Up And Down
 ---------------------------------
 
 At this point a "fresh start" is made by building a `Clock` which
-can count up and down, based on an class `UpDownCounter`.
+can count up and down.
+
+The former are build based on objects of `UpDownCounter`-s, which
+– as their name suggests – supports counting up and down too, not
+only when used "stand alone" but also when "chained" together.
+
+Intensive test code is supplied for both classes. (It may also
+be useful to understand the intended use of both of the classes.)
